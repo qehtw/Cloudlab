b35083884f37c2117d825d2573be594231da6af8
qehtw <maxgrynkiv@gmail.com>
2025-11-16 17:14:14 +0200

    Changing db

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config:
-	SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:22883300Max@database-1.clim62ig0e2i.eu-north-1.rds.amazonaws.com:3306/dblab"
+	SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:22883300Max@database-2.clim62ig0e2i.eu-north-1.rds.amazonaws.com:3306/dblab"
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
